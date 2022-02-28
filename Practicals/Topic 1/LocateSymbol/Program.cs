b4e7aae5cb736adacad92816fc865eeff46d3e1e
Tarang74 <tjanawalkar74@gmail.com>
2022-02-28 22:26:48 +1000

    Exercise 6

---
+++ Practicals/Topic 1/LocateSymbol/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LocateSymbol
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Keep the following line intact 
+            Console.WriteLine("===========================");
+
+            // Insert your solution here.
+            Console.WriteLine("Please enter a line of text:");
+            string input = Console.ReadLine();
+
+            int index1 = input.IndexOf('$');
+            int index2 = input.LastIndexOf('$');
+
+            Console.WriteLine($"The first and last positions of '$' are {index1} and {index2}, respectively.");
+
+            // Keep the following line intact 
+            Console.WriteLine("===========================");
+        }
+    }
+}
