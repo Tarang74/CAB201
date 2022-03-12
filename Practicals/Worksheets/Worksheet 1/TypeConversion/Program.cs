db9ae14fd86533c8ea20846f542f7714870be559
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2022-03-12 17:59:15 +1000

    Fix member access operator precedence

--- Practicals/Worksheets/Worksheet 1/TypeConversion/Program.cs
+++ Practicals/Worksheets/Worksheet 1/TypeConversion/Program.cs
@@ -21,7 +21,7 @@ namespace TypeConversion
 
             byte a = 80;
             Console.WriteLine("a) implicit");
-            Console.WriteLine($"\t{80} {80.GetType()} -> {a} {a.GetType()}");
+            Console.WriteLine($"\t{80} {(80).GetType()} -> {a} {a.GetType()}");
             
             // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/checked-and-unchecked
             int b_int = 300;
@@ -31,19 +31,19 @@ namespace TypeConversion
 
             double c = (double)400;
             Console.WriteLine("c) explicit as 400 is an integer by default");
-            Console.WriteLine($"\t{400} {400.GetType()} -> {c} {c.GetType()}");
+            Console.WriteLine($"\t{400} {(400).GetType()} -> {c} {c.GetType()}");
 
             int d = (int)400.25m;
             Console.WriteLine("d) explicit as int truncates floating-point values");
-            Console.WriteLine($"\t{400.25m} {400.25m.GetType()} -> {d} {d.GetType()}");
+            Console.WriteLine($"\t{400.25m} {(400.25m).GetType()} -> {d} {d.GetType()}");
 
             int e = (int)400.75f;
             Console.WriteLine("e) explicit as int truncates floating-point values");
-            Console.WriteLine($"\t{400.75f} {400.75f.GetType()} -> {e} {e.GetType()}");
+            Console.WriteLine($"\t{400.75f} {(400.75f).GetType()} -> {e} {e.GetType()}");
 
             decimal f = (decimal)721.63d;
             Console.WriteLine("f) explicit as decimals are represented differently to floats and doubles");
-            Console.WriteLine($"\t{721.63d} {721.63d.GetType()} -> {f} {f.GetType()}");
+            Console.WriteLine($"\t{721.63d} {(721.63d).GetType()} -> {f} {f.GetType()}");
         }
     }
 }
