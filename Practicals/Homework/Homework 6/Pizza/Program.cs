c474e26ba6b1a8100e6a754d9daf493564f687a0
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2022-04-30 16:44:55 +1000

    Finished week 7 lecture content

---
+++ Practicals/Homework/Homework 6/Pizza/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question1
+{
+    class Pizza
+    {
+        List<string> toppings;
+        int diameter;
+        double price;
+        public Pizza(List<string> _toppings, int _diameter, double _price)
+        {
+
+        }
+    }
+    class Program
+    {
+        public static void info(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n" + s + "\n");
+            Console.ResetColor();
+        }
+        public static void Main(string[] args)
+        {
+            info("Question 1:");
+
+            Pizza a = new Pizza();
+        }
+    }
+}
